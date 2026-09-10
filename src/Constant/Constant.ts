7875b6d6afedcd3b7e870c839e1edaceb7df44a7
flaviofabien <fabien4240flavio@gmail.com>
2026-09-11 00:39:39 +0300

    change Ip  backend

--- src/Constant/Constant.ts
+++ src/Constant/Constant.ts
@@ -1,5 +1,2 @@
-export const IP = "http://localhost:4000/api"
-export const IPLocal = "http://localhost:4000"
-
-// export const IP = "http://10.42.0.1:4000/api" 
-// export const IPLocal = "http://10.42.0.1:4000" 
+export const IP = "https://hotelhub-server-1.onrender.com/api"
+export const IPLocal = "https://hotelhub-server-1.onrender.com"
